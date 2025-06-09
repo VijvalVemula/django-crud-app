35ac444fd2f6fe23dc7ce2706744198ea1b74a7c
Vijval Vemula <vemulavijval1@gmail.com>
2025-06-09 18:17:14 -0400

    Use Default Db

--- crud_project/crud_project/settings.py
+++ crud_project/crud_project/settings.py
@@ -76,13 +76,9 @@ WSGI_APPLICATION = "crud_project.wsgi.application"
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "django-db",
-        "USER": "postgresadmin",
-        "PASSWORD": "Vijval#767485!",
-        "HOST": "django-db.cb9v6gj5sqme.us-east-1.rds.amazonaws.com",  # Will be updated after RDS setup
-        "PORT": "5432",
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
